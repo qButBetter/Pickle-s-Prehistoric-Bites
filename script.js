b7bb808a49ac909dab69bbf2f49d94ccd2e29917
James Steed <james.steed23@student.cart.org>
2023-11-29 09:48:22 -0800

    more coding

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ toggle between hiding and showing the dropdown content */
       }
     }
   }
+
+  // Carousel
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
